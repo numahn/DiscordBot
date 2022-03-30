1ed15e8400d3ea86ae0f19625361c07448ccab7c
Noman Ali <80441451+nali556@users.noreply.github.com>
2022-03-30 19:08:46 +0000

    Few comments added

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ token = os.environ['token']
 #Starting the bare minimum
 client = discord.Client()
 
+#Ready function
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))  
 
+#Hello!
 @client.event
 async def on_message(message):
   if message.author == client.user:
@@ -15,4 +17,4 @@ async def on_message(message):
   if message.content.startswith('&hello'):
     await message.channel.send('Hello! AUGHHH')
 
-client.run(token)
+client.run(token) #Runs the bot

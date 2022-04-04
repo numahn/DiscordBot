4cefbd4dded8ece0cff4ac4054c30d240608001c
Noman Ali <80441451+nali556@users.noreply.github.com>
2022-04-04 17:21:50 +0000

    Added &cat, random pic of cat posted

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import discord
 import os
+import requests
+import json
+
 token = os.environ['token']
 #Starting the bare minimum
 client = discord.Client()
 
+#Helper function
+def get_pic():
+  response = requests.get("https://api.thecatapi.com/v1/images/search")
+  json_data = json.loads(response.text)
+  url = json_data[0]['url']
+  return url
 #Ready function
 @client.event
 async def on_ready():
@@ -16,5 +25,8 @@ async def on_message(message):
     return
   if message.content.startswith('&hello'):
     await message.channel.send('Hello! AUGHHH')
-
+  if message.content.startswith('&carmen'):
+    await message.channel.send('hi my name is AUHHHH       winstead im AUGHHHHHH seventeen years old and i am very similar to you did i mention to you that i am AUGGHHHHHH group of girls pushed me down a sewer')
+  if message.content.startswith('&cat'):
+    await message.channel.send(get_pic())
 client.run(token) #Runs the bot
